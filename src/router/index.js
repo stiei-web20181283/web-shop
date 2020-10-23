cd76e8469c6d17b0d41feec573b85cbcde33ca01
Lexcubia <742681100@qq.com>
2020-10-23 17:42:29 +0800

    sass

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/views/layout/layout'
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+  {
+    path: '',
+    component: Layout,
+    redirect: 'home',
+    children: [
+      {
+        path: 'home',
+        component: () => import('@/views/home/index'),
+        name: 'Home',
+        meta: { title: 'home', icon: '', noCache: true }
+      }
+    ]
+  }
+]
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: constantRouterMap
 })
