8fdaa19cbe9f7a8712db2993582f2ae89f35b315
Lexcubia <742681100@qq.com>
2020-10-29 10:24:21 +0800

    remove docs

--- docs/static/js/5.92b272d9be9c7135c5c5.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"+H76":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("404")])},staticRenderFns:[]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
-//# sourceMappingURL=5.92b272d9be9c7135c5c5.js.map
